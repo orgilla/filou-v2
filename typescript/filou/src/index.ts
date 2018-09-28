bba1db9e99857473118fe9726dea24cdfef11ac7
Benjamin Kniffler <bkniffler@me.com>
2018-09-29 01:07:06 +0200

    fix fela

--- typescript/filou/src/index.ts
+++ typescript/filou/src/index.ts
@@ -2,3 +2,4 @@
 export { FelaComponent } from 'react-fela';
 export { default as Grid } from '@filou/grid';
 export { default as createRenderer } from './create-renderer';
+export { default as Provider } from './provider';
