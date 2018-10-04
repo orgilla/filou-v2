cd77b06fb417a64fb2a9f7cdfbd6b87d0d2996a4
Benjamin Kniffler <bkniffler@me.com>
2018-10-04 18:08:47 +0200

    exprt filou menu, add es/lib to ignore, clean

--- typescript/filou/src/index.ts
+++ typescript/filou/src/index.ts
@@ -10,3 +10,4 @@ export { default as Grid } from '@filou/grid';
 export { default as Ribbon } from '@filou/ribbon';
 export { default as ElectronRouter } from '@filou/electron-router';
 export { default as Tabs } from '@filou/tabs';
+export { default as Menu } from '@filou/menu';
