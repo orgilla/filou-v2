c8f79d492410ec185695801ce4e70613a44aafee
Benjamin Kniffler <bkniffler@me.com>
2018-10-04 13:33:59 +0200

    add divider to test

--- typescript/ribbon/stories/ribbon.stories.tsx
+++ typescript/ribbon/stories/ribbon.stories.tsx
@@ -19,10 +19,15 @@ import Ribbon from '../src';
     <Ribbon width="100%" height="500px">
       <Ribbon.Title maximized os={Ribbon.OSStyle.MAC} brand="Mac Ribbon">
         <Ribbon.Item>Left Item</Ribbon.Item>
+        <Ribbon.Divider />
+        <Ribbon.Item>Left Item</Ribbon.Item>
+        <Ribbon.Item>Left Item</Ribbon.Item>
         <Ribbon.Space />
         <Ribbon.Item>Right Item</Ribbon.Item>
       </Ribbon.Title>
-      <Ribbon.Actions>
+      <Ribbon.Actions title="Actions">
+        <Ribbon.Item>Hallo</Ribbon.Item>
+        <Ribbon.Divider />
         <Ribbon.Item>Hallo</Ribbon.Item>
         <Ribbon.Space />
       </Ribbon.Actions>
@@ -72,10 +77,15 @@ import Ribbon from '../src';
       <Ribbon width="800px" height="500px">
         <Ribbon.Title os={Ribbon.OSStyle.WIN} brand="Windows Ribbon">
           <Ribbon.Item>Left Item</Ribbon.Item>
+          <Ribbon.Divider />
+          <Ribbon.Item>Left Item</Ribbon.Item>
+          <Ribbon.Item>Left Item</Ribbon.Item>
           <Ribbon.Space />
           <Ribbon.Item>Right Item</Ribbon.Item>
         </Ribbon.Title>
         <Ribbon.Actions title="Actions">
+          <Ribbon.Item>Hallo</Ribbon.Item>
+          <Ribbon.Divider />
           <Ribbon.Item>Hallo</Ribbon.Item>
           <Ribbon.Space />
         </Ribbon.Actions>
