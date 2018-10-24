be4e9a2e3f776b632a9c5f57d7abe5d0749513df
Benjamin Kniffler <bkniffler@me.com>
2018-10-24 20:33:46 +0200

    small fix :(

--- typescript/filou-web/src/react-static/index.tsx
+++ typescript/filou-web/src/react-static/index.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import { Provider } from '@filou/core';
-import { Document } from 'filou-web';
+import Document from '../document';
 const { renderToSheetList } = require('fela-dom');
 
 export const createReactStaticConfig = (config: any) => ({
