ba05e13aa2bbe1c3f2d79b39be6434d0ab962604
Philipp Achenbach <github@philipp-achenbach.de>
2018-11-03 08:30:30 +0100

    dix fela-theme

--- typescript/core/src/fela-theme.tsx
+++ typescript/core/src/fela-theme.tsx
@@ -1,12 +1,12 @@
-import * as React from "react";
-import { FelaTheme as FelaTh } from "react-fela";
+import * as React from 'react';
+import { FelaTheme as FelaTh } from 'react-fela';
 
 interface IFelaTheme {
   render: (theme: any) => React.ReactNode;
 }
 
-const FelaTheme: React.SFC<IFelaTheme> = () => {
-  return <FelaTh />;
+const FelaTheme: React.SFC<IFelaTheme> = props => {
+  return <FelaTh {...props} />;
 };
 
 export default FelaTheme;
