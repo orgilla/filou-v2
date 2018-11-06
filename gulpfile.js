93cfba5a8f5c8bb7e2825e0c712bbeebb1dfeba7
Benjamin Kniffler <bkniffler@me.com>
2018-11-06 14:10:36 +0100

    gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,32 +1,45 @@
 const gulp = require('gulp');
+const merge = require('merge-stream');
 // const sourcemaps = require('gulp-sourcemaps');
 const plumber = require('gulp-plumber');
 const newer = require('gulp-changed');
 const babel = require('gulp-babel');
 const watch = require('gulp-watch');
 const debug = require('gulp-debug');
+const { lstatSync, readdirSync } = require('fs');
+const { resolve } = require('path');
+
+const isDirectory = source => lstatSync(source).isDirectory();
+const getDirectories = source =>
+  readdirSync(source)
+    .map(name => resolve(source, name))
+    .filter(isDirectory);
 
 process.env.NODE_ENV = 'production';
 
-const dest = 'lib';
-const src = ['typescript/**/src/**/*'];
-
-const compile = (glp, force) =>
-  glp
-    .pipe(plumber())
-    .pipe(debug())
-    .pipe(newer(dest, { extension: force ? '.xyz' : '.js' }))
-    // .pipe(sourcemaps.init())
-    .pipe(
-      babel({
-        presets: ['@babel/typescript']
-      })
-    )
-    // .pipe(sourcemaps.write('.'))
-    .pipe(gulp.dest(dest));
+const compile = (glp, force) => {};
 
 gulp.task('watch', () => {
-  return compile(watch(src, { ignoreInitial: false, base: dest, dot: true }));
+  const force = false;
+  const tasks = getDirectories('./typescript').map(p => {
+    const src = resolve(p, 'src', '**/*');
+    const dest = resolve(p, 'lib');
+    return (
+      watch(src, { ignoreInitial: false, dot: true })
+        .pipe(plumber())
+        .pipe(debug())
+        .pipe(newer(dest, { extension: force ? '.xyz' : '.js' }))
+        // .pipe(sourcemaps.init())
+        .pipe(
+          babel({
+            presets: ['@babel/typescript']
+          })
+        )
+        // .pipe(sourcemaps.write('.'))
+        .pipe(gulp.dest(dest))
+    );
+  });
+  return merge(tasks);
 });
 
 gulp.task('build', () => {
