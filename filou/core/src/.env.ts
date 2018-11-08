14de918d4a46e3da9454312c52e271844881b5b5
Benjamin Kniffler <bkniffler@me.com>
2018-11-08 12:00:27 +0100

    bump

---
+++ filou/core/src/.env.ts
@@ -0,0 +1,22 @@
+import * as React from 'react';
+import { object } from 'prop-types';
+
+interface FelaStaticProps {
+  css: string;
+}
+
+export class FelaStatic extends React.Component<FelaStaticProps> {
+  static contextTypes = {
+    renderer: object
+  };
+  static displayName = 'FelaStatic';
+  constructor(props: FelaStaticProps, context: any) {
+    super(props, context);
+    this.context.renderer.renderStatic(props.css);
+  }
+  render() {
+    return React.Children.only(this.props.children);
+  }
+}
+
+export default FelaStatic;
